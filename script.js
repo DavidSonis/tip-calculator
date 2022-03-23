62ac23b9d311e1bb21dcfd123cb128ae1efa1fe4
David Sonis <92735576+DavidSonis@users.noreply.github.com>
2022-03-23 10:25:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function calcTip(){
+/* --------------------------------Step 1: grab the input values------------------------------------------------------ */
+  let tipValue = document.getElementById('tipForm').value;
+  let billValue = document.getElementById('billInput').value;                                                      //sets a variable for the tip Amount selector to the VALUE of the #tipForm selector
+                                                          //sets a variable for the bill amount selector using the VALUE of the #billInput selector
+
+
+/* ---------------------------------------Step 2: turn strings into numbers, use parseFloat() method to do this.-----------  IE: let vairable = parseFloat(stringVariable)----------------------------- */
+  let tipAmount = parseFloat(tipValue);
+  let billAmount = parseFloat(billValue);                                                        // sets a variable for the Tip Amount that is a number
+                                                          // sets a variable for the Bill Amount that is a number
+
+/* --------------------------------Step 3: grab the elements---------------------------------------------------------- */
+                                                          // sets a var to the ID of 'bill-p'
+    let bill = document.getElementById('bill-p');
+    let tip = document.getElementById('tip-p');
+    let total = document.getElementById('total-p');                                                         // sets a var to the ID of 'tip-p'
+                                                          // sets a var to the ID of 'total-p'
+
+/* --------------------------------Step 4: Place your calculations here----------------------------------------------- */
+                                                          // calulate total tip (tip % * bill) and store it in the a new variable
+let totalTip = tipAmount * billAmount;
+let totalValue = totalTip + billAmount;                                                      // calcualte total bill (totalTip + billAmount) and store it in a new variable
+
+
+/* --------------------------------Step 5: Append your HTML content with the new information-----**use .fixed(2) to display the value with 2 decimal places.-------------------- */
+                                                          // change the innerHTML of the tip var from step 3 to the total tip from step 4
+  total.innerHTML = "$" + totalValue.toFixed(2);
+  tip.innerHTML = "$" + totalTip.toFixed(2);
+  bill.innerHTML = "$" + billAmount.toFixed(2);                                                       // change the innterHTML of the bill var from step 3 to the total bill var from step 2
+                                                          // change the innerHTML of the total var from step 3 to the total bill var from step 4
+
+
+
+}
